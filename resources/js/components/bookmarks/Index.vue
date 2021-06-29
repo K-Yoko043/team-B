269d8084b15f9277e5edf7889351b8e0530a34d1
yokoyama_naoya <naoya.yokoyama@mountain-gorilla.co.jp>
2021-06-29 16:11:03 +0900

    ブックマークページに戻るボタン、プルダウン追加

--- resources/js/components/bookmarks/Index.vue
+++ resources/js/components/bookmarks/Index.vue
@@ -1,91 +1,83 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body" v-cloak>
-                        <div class="d-flex justify-content-start mb-3">
-                            <!-- <div class="align-self-center mr-3">
-                            <button type="button" class="btn btn-primary" @click="onCreate"><i class="fas fa-plus"></i> 新規追加</button>
-                        </div> -->
-                            <div class="dropdown text-right">
-                                <button
-                                    class="btn btn-outline-dark dropdown-toggle"
-                                    type="button"
-                                    id="dropdownMenuButton"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                >
-                                    <i class="fas fa-sliders-h"></i>
-                                </button>
-                                <div
-                                    class="dropdown-menu"
-                                    aria-labelledby="dropdownMenuButton"
-                                >
-                                    <a class="dropdown-item" href="#">
-                                        <router-link :to="{ name: 'content' }"
-                                            >新規投稿</router-link
-                                        >
-                                    </a>
-                                    <a class="dropdown-item" href="./">HOME</a>
-                                </div>
-                            </div>
+        <div class="card text-center mx-auto" style="max-width: 800px">
+            <div class="fixed-top">
+                <div class="form-inline justify-content-center">
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="d-flex flex-wrap justify-content-center mb-2" style="float:left">
+                    <button class="btn btn-outline-dark" onclick="location.href='./'">戻る</button>
+                </div>
 
-                            <div class="align-self-center">
-                                <!-- <button type="button" class="btn btn-primary btn-menu mr-3 mb-3" @click="onBack">戻る</button> -->
-                            </div>
-                        </div>
-                        
-                        <div v-if="check === 1">
-                            <div v-for="content in contents" :key="content.id">
-                                <div v-if="content.is_bookmark === 1">
-                                    <h3 class="card-header">
-                                        <img
-                                            src="/storage/1623910044.柴犬.jpg"
-                                            class="img-thumbnail"
-                                            alt=""
-                                        />
-                                        {{ content.user_name }}
-                                        <i class="far fa-edit clickable"> </i>
-                                        <i
-                                            v-if="content.is_bookmark == 0"
-                                            class="far fa-bookmark"
-                                            @click="addbook(content.id)"
-                                            style="color:#04B4AE;float:right;"
-                                        ></i>
-                                        <i
-                                            v-else
-                                            class="fas fa-bookmark"
-                                            @click="deletebook(content.id)"
-                                            v-cloak
-                                            style="color:#04B4AE;float:right;"
-                                        ></i>
-                                    </h3>
-                                    <div class="card-body">
-                                        <h5
-                                            class="card-subtitle mb-2 text-muted"
-                                        >
-                                            {{ content.tag }}
-                                        </h5>
-                                        <p
-                                            class="card-text text-left"
-                                            style="white-space: pre-wrap;"
-                                        >
-                                            {{ content.content_text }}
-                                        </p>
-                                    </div>
-                                    <p>投稿日時：{{ content.created_at }}</p>
-                                </div>
-                                
-                            </div>
-                            <hr>
-                        </div>
-                        <div v-else v-cloak>
-                            <p v-cloak>{{ error_message }}</p>
+                <div class="dropdown" style="float:right">
+                        <button
+                            class="btn btn-outline-dark dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuButton"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            <i class="fas fa-sliders-h"></i>
+                        </button>
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="dropdownMenuButton"
+                        >
+                            <a class="dropdown-item" href="#">
+                                <router-link :to="{ name: 'content' }"
+                                    >新規投稿</router-link
+                                >
+                            </a>
+                            <a class="dropdown-item" href="./">トップ</a>
                         </div>
                     </div>
+            </div>
+
+            <table class="table table-striped" v-if="check === 1">
+                <div
+                    v-for="content in contents"
+                    :key="content.id"
+                    class="card bg-white border-info"
+                >
+                    <h3 class="card-header">
+                        <img
+                            src="/storage/1623910044.柴犬.jpg"
+                            class="img-thumbnail"
+                            alt=""
+                        />
+                        {{ content.user_name }}
+                        <i
+                            v-if="content.is_bookmark == 0"
+                            class="far fa-bookmark"
+                            @click="addbook(content.id)"
+                            style="color:#04B4AE; float:right;"
+                        ></i>
+                        <i
+                            v-else
+                            class="fas fa-bookmark"
+                            @click="deletebook(content.id)"
+                            v-cloak
+                            style="color:#04B4AE; float:right;"
+                        ></i>
+                    </h3>
+                    <div class="card-body">
+                        <h5 class="card-subtitle mb-2 text-muted">
+                            {{ content.tag }}
+                        </h5>
+                        <p
+                            class="card-text text-left"
+                            style="white-space: pre-wrap;"
+                        >
+                            {{ content.content_text }}
+                        </p>
+                    </div>
+                    <p>投稿日時：{{ content.created_at }}</p>
                 </div>
+            </table>
+            <div v-else v-cloak>
+                <p v-cloak>{{ error_message }}</p>
             </div>
         </div>
     </div>
@@ -94,7 +86,9 @@
 <script>
 import moment from "moment";
 export default {
-    props: [],
+    props: {
+        //
+    },
     data() {
         return {
             contents: [],
@@ -105,11 +99,14 @@ export default {
         };
     },
     mounted() {
-        //this.inspected_on = new moment().format('YYYY-MM-DD')
         this.getItems();
     },
-    watch: {},
-    computed: {},
+    watch: {
+        //
+    },
+    computed: {
+        //
+    },
     methods: {
         getItems: function() {
             this.isLoading = true;
@@ -117,7 +114,6 @@ export default {
             axios.all([api.get("/api/Bookmark/")]).then(
                 axios.spread((res1, res2, res3, res4) => {
                     this.contents = res1.data;
-                    console.log(this.contents);
 
                     for (let i in this.contents) {
                         if (this.contents[i].is_bookmark === 1) {
@@ -137,7 +133,6 @@ export default {
                 })
             );
         },
-
         deletebook: function(contentid) {
             axios.delete("/api/content/delete/" + contentid);
             location.reload();
@@ -145,9 +140,6 @@ export default {
         onBack: function() {
             this.$router.go(-1);
         }
-    },
-    components: {
-        // Loading
     }
 };
 </script>
@@ -173,6 +165,7 @@ export default {
 .card {
     margin-bottom: 50px;
 }
+
 
 [v-cloak] {
     display: none;
