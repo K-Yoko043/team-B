04ae4416315ff0d7d18f70ea36aa757249ec2d2b
K-Yoko043 <83930921+K-Yoko043@users.noreply.github.com>
2021-07-03 09:55:25 +0900

    画像だけ消しました

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -71,7 +71,6 @@
 				class="card bg-white border-info"
 			>
 				<h3 class="card-header">
-					<img src="/storage/1623910044.柴犬.jpg" class="img-thumbnail" alt="">
 					{{ content.user_name }}
 					<i v-if="content.user_name === own.name"
 						 class="far fa-edit clickable" @click="onResume(content)">
